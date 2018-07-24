df88aaa52ab7a12043e78cc85a104fb8d3b5f1b5
Joshua Ernzen <josh@elem14.tech>
2018-07-24 17:42:04 -0500

    Tells players whos turn it is.

--- monolopy.py
+++ monolopy.py
@@ -12,28 +12,37 @@ for i in range(0, number_of_players):
 
 print("\n" * 100)
 
+turn = 0
+
 while True:
     jail_count = 0
 
     while True:
+        if turn >= number_of_players:
+            turn = 0
+
+        print("It's", player_name[turn], "\b's turn.")
+
         first_dice = randint(1, 6)
         second_dice = randint(1, 6)
+
         print("\n" * 5)
 
         print("First dice is:", first_dice)
         print("Second dice is:", second_dice)
 
         if first_dice != second_dice:
+            turn += 1
             break
 
         jail_count += 1
 
+        if jail_count < 3:
+            print("You rolled doubles! Play again!")
+
         if jail_count == 3:
             print("Go to jail!")
             break
-
-        elif jail_count < 3:
-            print("You rolled doubles! Play again!")
 
             if jail_count == 2:
                 print("If you roll doubles again, you'll go to jail!")
