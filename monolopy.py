90ce33c40d618501aa266204ba2c437ce74646ff
Joshua Ernzen <josh@elem14.tech>
2018-07-27 13:33:30 -0500

    Implemented jail counter to keep track of who's in jail and for how long.

--- monolopy.py
+++ monolopy.py
@@ -2,6 +2,8 @@
 
 from random import randint
 
+player_name = []
+
 class PlayerName:
     def __init__(self, name, double_count, jail_count, in_jail):
         self.name = name
@@ -9,7 +11,28 @@ class PlayerName:
         self.jail_count = jail_count
         self.in_jail = in_jail
 
-player_name = []
+def jail_dice_roll(player):
+    print(player_name[player].name, "is in jail!")
+
+    if player_name[player].jail_count < 2:
+        first_dice = randint(1, 6)
+        second_dice = randint(1, 6)
+
+        print("First dice is:  ", first_dice)
+        print("Second dice is: ", second_dice)
+
+        if first_dice == second_dice:
+            print(player_name[player].name, "got out of jail!")
+            return False
+
+        else:
+            player_name[player].jail_count += 1
+            return True
+
+    else:
+        print("You must pay $50 to get out now.")
+        return False
+
 print("\n" * 100)
 number_of_players = int(input("How many players?\n"))
 
@@ -22,11 +45,19 @@ print("\n" * 100)
 turn = 0
 
 while True:
-    jail_count = 0
-
     while True:
         if turn >= number_of_players:
             turn = 0
+
+        if player_name[turn].in_jail == True:
+
+            print("\n" * 100)
+
+            if jail_dice_roll(turn) == False:
+                player_name[turn].in_jail = False
+
+            turn += 1
+            break
 
         first_dice = randint(1, 6)
         second_dice = randint(1, 6)
@@ -39,18 +70,21 @@ while True:
         print("Second dice is:", second_dice)
 
         if first_dice != second_dice:
+            player_name[turn].double_count = 0
             turn += 1
             break
 
-        jail_count += 1
+        player_name[turn].double_count += 1
 
-        if jail_count < 3:
+        if player_name[turn].double_count < 3:
             print("You rolled doubles! Play again!")
 
-            if jail_count == 2:
+            if player_name[turn].double_count == 2:
                 print("If you roll doubles again, you'll go to jail!")
 
-        if jail_count == 3:
+        if player_name[turn].double_count >= 3:
+            player_name[turn].in_jail = True
+            player_name[turn].double_count = 0
             print("Go to jail!")
             turn += 1
             break
